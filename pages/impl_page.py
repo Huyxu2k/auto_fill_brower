5599bc06b935b7f2e120ba61199dcf73656f7194
huynv <huynv@phucthanhgroup.com>
2026-03-03 22:35:58 +0700

    update impl page

--- pages/impl_page.py
+++ pages/impl_page.py
@@ -1,6 +1,27 @@
 from selenium.webdriver.common.by import By
 from pages.base import BaseForm
+from models.person import Person
 
 class TestPage(BaseForm):
-    SEARCH_INPUT = (By.CSS_SELECTOR, '#search')
+    URL = "https://dichvucong.gov.vn/p/home/dvc-tra-cuu-ho-so.html"
+    # textbox
+    MA_HO_SO = (By.ID, 'maHSB')
+    MA_BAO_MAT = (By.ID, 'ipCaptchaB')
     
+    # button
+    TRA_CUU = (By.ID, 'btnSubmitB')
+    
+    def open(self):
+        self.browser.get(self.URL)
+
+    def fill(self, person: Person):
+        input_ma_hs = self.get(self.MA_HO_SO)
+        input_ma_hs.clear()
+        input_ma_hs.send_keys(person.name)
+
+        input_captcha = self.get(self.MA_BAO_MAT)
+        input_captcha.clear()
+        input_captcha.send_keys(person.yob)
+
+    def click(self):
+        self.click(self.TRA_CUU)
