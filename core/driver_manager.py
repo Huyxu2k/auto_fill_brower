334c410a3da1bf016a94d2c07c6ff0849ba4472d
huynv <huynv@phucthanhgroup.com>
2026-03-08 22:37:24 +0700

    add ui for tool

--- core/driver_manager.py
+++ core/driver_manager.py
@@ -5,11 +5,13 @@ from loguru import logger
 import os
 
 class DriverManager:
-    PROFILE_PATH = r"C:\Users\huynv\AppData\Local\Google\Chrome\User Data\Profile 2"
+    #OFILE_PATH = r"C:\Users\huynv\AppData\Local\Google\Chrome\User Data\Profile 2"
+    PROFILE_PATH = r"C:\Users\Admin\AppData\Local\Google\Chrome\User Data\Profile 10"
 
     def __init__(self, profile_path: str | None = None):
         self.profile_path = profile_path or self.PROFILE_PATH
         self.headless = False
+        self.driver = None  # Thêm biến để lưu trữ driver hiện tại
 
     def create_driver(self):
         options = Options()
@@ -21,6 +23,8 @@ class DriverManager:
         else:
             logger.warning("Không tìm thấy profile_path hoặc đường dẫn trống. Khởi tạo Chrome mới.")
 
+        # Giữ trình duyệt mở sau khi script kết thúc
+        options.add_experimental_option("detach", True)
         options.add_argument("--start-maximized")
         options.add_argument("--disable-notifications")
 
@@ -36,3 +40,6 @@ class DriverManager:
         driver.implicitly_wait(5)
 
         return driver
+
+    def close():
+        return
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from models.person import Person
 
 def main():
     #1. Đọc dữ liệu từ nguồn
-    reader_manager = ReaderManager(r"C:\Users\huynv\Desktop\Test.xlsx")
+    reader_manager = ReaderManager(r"C:\Users\Admin\Desktop\test.xlsx")
     persons: list[Person] = reader_manager.read_excel()
 
     if len(persons) == 0:
@@ -21,11 +21,59 @@ def main():
     #3. Chạy test
     test_page = TestPage(driver)
     test_page.open()
+    test_page.wait(5)
     test_page.fill(persons[0])
+    test_page.wait(5)
     test_page.click_btn()
     #4. Loop
 
     logger.info(f"Hoàn thành !")
+
+
+# giao diện 
+
+import sys
+import threading
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
+                            QHBoxLayout, QPushButton, QLineEdit, QTextEdit,
+                            QLabel, QFileDialog)
+from PyQt6.QtCore import Qt, pyqtSignal, QObject
+
+
+class LogHandler(QObject):
+    # Đẩy log lên giao diện PyQt6
+    n_log = pyqtSignal(str)
+
+    def write(self, message):
+        if message.strip():
+            self.n_log.emit(message.strip())
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+    def init_ui(self):
+       #TODO
+       return 
+    def browse_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Chọn file dữ liệu", "", "Excel Files (*.xlsx *.xls)")
+        if file_path:
+            self.txt_file_path.setText(file_path)
+
+    def update_log_ui(self, message):
+        self.log_output.append(message)
+        # Tự động cuộn xuống cuối
+        self.log_output.moveCursor(self.log_output.textCursor().MoveOperation.End)
+    
+    def start(self):
+        return
+    
+    def run_process(self, path):
+        return
     
 if __name__ == "__main__":
-    main()
+    #main()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
