662a1fa9a6a9067527ba9fd95985622625075732
Huyxu2k <nguyenvanhuy2712000@gmail.com>
2026-03-12 14:12:47 +0700

    update loader

--- core/loader_manager.py
+++ core/loader_manager.py
@@ -7,10 +7,18 @@ from models.step import AutomationSteps, Step
 
 class LoaderManager:
     def __init__(self, json_path: str):
-        # check path, check định dạng file
         self.json_path = json_path
 
     def load_config(self) -> Optional[AutomationSteps]:
-        # Map đối tượng từ joson sang Step và add vào AutomationSteps
-        # TODO
-        return
+        try:
+            # check path, check định dạng file
+
+            # Map đối tượng từ joson sang Step và add vào AutomationSteps
+            # TODO
+            return
+        except FileNotFoundError:
+            logger.error("Không tìm thấy file {self.json_path}")
+            return None
+        except json.JSONDecodeError:
+            logger.error("File không đúng định dạng")
+            return None
